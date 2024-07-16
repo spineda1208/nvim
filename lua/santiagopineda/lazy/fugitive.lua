1cdf7ce0ce6c3391f505fcaebbce36cb141ad89b
spineda1208 <spineda1208@gmail.com>
2024-07-16 15:05:25 -0400

    update: fugitive merge conflicts keybinds

--- lua/santiagopineda/lazy/fugitive.lua
+++ lua/santiagopineda/lazy/fugitive.lua
@@ -5,7 +5,7 @@ return {
 		vim.keymap.set("n", "<leader>gps", ":Git push<CR>")
 		vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>")
 		vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>")
-		vim.keymap.set("n", "<leader>gj", ":diffget //2<CR>")
-		vim.keymap.set("n", "<leader>gk", ":diffget //3<CR>")
+		vim.keymap.set("n", "<leader>gh", ":diffget //2<CR>")
+		vim.keymap.set("n", "<leader>gl", ":diffget //3<CR>")
 	end,
 }
