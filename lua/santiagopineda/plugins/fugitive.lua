03ec6b11b556323b898d4a1de7fff8cbfbcf3c00
spineda1208 <spineda1208@gmail.com>
2024-10-05 22:40:57 -0400

    update: remaps for fugitive

--- lua/santiagopineda/plugins/fugitive.lua
+++ lua/santiagopineda/plugins/fugitive.lua
@@ -7,6 +7,7 @@ return {
 		vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>")
 		vim.keymap.set("n", "<leader>gw", ":Gwrite<CR>")
 		vim.keymap.set("n", "<leader>gh", ":diffget //2<CR>")
-		vim.keymap.set("n", "<leader>gl", ":diffget //3<CR>")
+		vim.keymap.set("n", "<leader>gk", ":diffget //3<CR>")
+		vim.keymap.set("n", "<leader>gl", ":Git log --oneline --graph --decorate --parents<CR>")
 	end,
 }
