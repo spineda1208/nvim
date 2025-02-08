7bcd065b51418dff6c00a6887311d4065c617bf6
spineda1208 <spineda1208@gmail.com>
2025-02-08 11:20:16 -0500

    feat(cmp): migrated nvim-cmp to use blink.cmp

--- lua/santiagopineda/plugins/lsp.lua
+++ lua/santiagopineda/plugins/lsp.lua
@@ -2,73 +2,93 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
+			{
+				"saghen/blink.cmp",
+
+				dependencies = "rafamadriz/friendly-snippets",
+
+				version = "*",
+
+				opts = {
+					keymap = {
+						preset = "default",
+						["<CR>"] = { "accept", "fallback" },
+						[";"] = { "show_documentation", "hide_documentation" },
+					},
+					appearance = {
+						use_nvim_cmp_as_default = true,
+						nerd_font_variant = "mono",
+					},
+					signature = { enabled = true },
+				},
+			},
+
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/nvim-cmp",
-			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-cmdline",
-			"hrsh7th/cmp-path",
-			"hrsh7th/cmp-nvim-lsp",
+			{
+				"folke/lazydev.nvim",
+				dependencies = {
+					{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+				},
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "luvit-meta/library", words = { "vim%.uv" } },
+					},
+				},
+			},
 			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
-			"j-hui/fidget.nvim",
 			"artemave/workspace-diagnostics.nvim",
 		},
+
 		config = function()
-			local cmp = require("cmp")
-			local cmp_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.tbl_deep_extend(
-				"force",
-				{},
-				vim.lsp.protocol.make_client_capabilities(),
-				cmp_lsp.default_capabilities()
-			)
-			local universal_lsp_config = {
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
+			local config = {
 				capabilities = capabilities,
+				autostart = true,
 				on_attach = function(client, bufnr)
 					require("workspace-diagnostics").populate_workspace_diagnostics(client, bufnr)
 				end,
 			}
 
-			require("fidget").setup({})
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				automatic_installation = {
+				automatic_installation = false,
+				ensure_installed = {
 					"lua_ls",
 					"ts_ls",
 					"pyright",
 				},
 				handlers = {
 					function(server_name) -- default handler (optional)
-						require("lspconfig")[server_name].setup(universal_lsp_config)
-					end,
-
-					["lua_ls"] = function()
-						local lspconfig = require("lspconfig")
-						lspconfig.lua_ls.setup({
-							capabilities = capabilities,
-							settings = {
-								Lua = {
-									runtime = { version = "Lua 5.1" },
-									diagnostics = {
-										globals = {
-											"bit",
-											"vim",
-											"it",
-											"describe",
-											"before_each",
-											"after_each",
-										},
-									},
-								},
-							},
-						})
+						require("lspconfig")[server_name].setup(config)
 					end,
 				},
 			})
-			require("lspconfig").gleam.setup(universal_lsp_config)
 
-			require("lspconfig").ocamllsp.setup(vim.tbl_extend("force", universal_lsp_config, {
+			require("lspconfig").lua_ls.setup(vim.tbl_extend("force", config, {
+				settings = {
+					Lua = {
+						runtime = { version = "Lua 5.1" },
+						diagnostics = {
+							globals = {
+								"bit",
+								"vim",
+								"it",
+								"describe",
+								"before_each",
+								"after_each",
+							},
+						},
+					},
+				},
+			}))
+
+			require("lspconfig").gleam.setup(config)
+
+			require("lspconfig").ocamllsp.setup(vim.tbl_extend("force", config, {
 				manual_install = true,
 				cmd = { "dune", "tools", "exec", "ocamllsp" },
 				capabilities = capabilities,
@@ -79,7 +99,7 @@ return {
 				},
 			}))
 
-			require("lspconfig").ruff.setup(vim.tbl_extend("force", universal_lsp_config, {
+			require("lspconfig").ruff.setup(vim.tbl_extend("force", config, {
 				manual_install = true,
 				cmd = { "ruff", "server" },
 				capabilities = capabilities,
@@ -87,52 +107,6 @@ return {
 					return vim.fn.getcwd()
 				end,
 			}))
-
-			local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "path" },
-					{ name = "luasnip" },
-				}, { { name = "buffer" } }),
-				-- formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-					["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp.mapping.confirm({ select = true }),
-					["<C-Space>"] = cmp.mapping.complete(),
-				}),
-			})
 		end,
 	},
-	{
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-	{ -- optional completion source for require statements and module annotations
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-		end,
-	},
-	-- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
