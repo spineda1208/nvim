9fc6bbd63075068376dc39b92668d534a9f6d663
spineda1208 <spineda1208@gmail.com>
2025-03-18 13:12:16 -0400

    update: lazy-lock

--- snippets/python.lua
+++ snippets/python.lua
@@ -5,14 +5,14 @@ local fmta = require("luasnip.extras.fmt").fmta
 
 return {
 	-- Imports
-	snippet("fmp", fmta("from <> import <>", { i(1, "module"), i(2, "...") })),
-	snippet("ima", fmta("import <> as <>", { i(1, "module"), i(2, "abv") })),
+	-- snippet("fmp", fmta("from <> import <>", { i(1, "module"), i(2, "...") })),
+	-- snippet("ima", fmta("import <> as <>", { i(1, "module"), i(2, "abv") })),
 	-- Functions
-	snippet("fn", fmta("def <>(<>):", { i(1, "function"), i(2, "parameters") })),
+	-- snippet("fn", fmta("def <>(<>):", { i(1, "function"), i(2, "parameters") })),
 	-- snippet("fnt", fmta("def <>(<>) -> <>:", { i(1, "function"), i(2, "parameters"), i(3, "return_type") })),
 	-- Loops
-	snippet("forr", fmta("for i in range(<>):", { i(1) })),
-	snippet("fori", fmta("for <> in <>:", { i(1, "item"), i(2, "iterable") })),
+	-- snippet("forr", fmta("for i in range(<>):", { i(1) })),
+	-- snippet("fori", fmta("for <> in <>:", { i(1, "item"), i(2, "iterable") })),
 	-- Conditionals
 	snippet(
 		{ trig = "iff", snippetType = "autosnippet" },
@@ -25,6 +25,6 @@ return {
 	),
 	snippet("ifi", fmta("if not isinstance(<>, <>):", { i(1, "variable"), i(2, "type") })),
 	-- Errors
-	snippet({ trig = "rte", snippetType = "autosnippet" }, fmta('raise TypeError("<>")', { i(0) })),
-	snippet({ trig = "rve", snippetType = "autosnippet" }, fmta('raise ValueError("<>")', { i(0) })),
+	-- snippet({ trig = "rte", snippetType = "autosnippet" }, fmta('raise TypeError("<>")', { i(0) })),
+	-- snippet({ trig = "rve", snippetType = "autosnippet" }, fmta('raise ValueError("<>")', { i(0) })),
 }
