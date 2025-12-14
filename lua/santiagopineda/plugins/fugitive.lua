1f5756ff4b1647c8ed02cce9d35f115199a1518e
spineda1208 <spineda1208@gmail.com>
2025-12-14 16:36:02 -0500

    feat: added gam shortcut + got rid of neofromat autocmd

--- lua/santiagopineda/plugins/fugitive.lua
+++ lua/santiagopineda/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gam", ":Git commit --amend --no-edit<CR>")
 		vim.keymap.set("n", "<leader>gps", ":Git push<CR>")
 		vim.keymap.set("n", "<leader>get", ":Git fetch<CR>")
 		vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>")
--- lua/santiagopineda/plugins/neoformat.lua
+++ lua/santiagopineda/plugins/neoformat.lua
@@ -3,6 +3,6 @@ return {
 	config = function()
 		vim.g.neoformat_try_node_exe = 1
 		vim.g.neoformat_enabled_python = { "ruff" }
-		vim.cmd("autocmd BufWritePre * Neoformat")
+		-- vim.cmd("autocmd BufWritePre * Neoformat")
 	end,
 }
